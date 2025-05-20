431c93b811781caa9bc164d36322043d8f0ec466
yasminpreciato <yasminfpreciato2019@gmail.com>
2025-05-19 22:32:55 -0300

    Criando banner

--- flix/src/app/pages/home/home.component.ts
+++ flix/src/app/pages/home/home.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MovieApiService } from '../../services/movie-api.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private service: MovieApiService) { };
 
+  bannerResults: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+  // Banner
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      //console.log(result.results);
+      this.bannerResults = result.results;
+    })
+  }
 }
--- flix/src/app/services/movie-api.service.ts
+++ flix/src/app/services/movie-api.service.ts
@@ -9,5 +9,20 @@ export class MovieApiService {
 
   constructor(private http: HttpClient) { }
 
+  baseUrl = 'https://api.themoviedb.org/3';
+  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjU1OGZhZDZmNGY1OGFiMzE2NGQwZmMzMjFlYjIzMyIsIm5iZiI6MTc0NzI0NjU2Ni42MTIsInN1YiI6IjY4MjRkZGU2MGQ4YzA2Y2U4ODRiNTEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hyDFk6_51kHEQnG2Vm4nMSULw7PiZmQcqLMk4dLjw4Q'
+    }
+  };
+
+  // Dados para o banner - midias em destaques da semana
+  bannerApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
+
   
+
 }
